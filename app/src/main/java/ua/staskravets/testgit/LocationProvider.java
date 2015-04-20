f3d92ffd26f28079b68b6d3966965157e5b06ae6
Stas Kravets <stas.kravets@nixsolutions.com>
2015-04-20 18:00:56 +0300

    commit 4

---
+++ app/src/main/java/ua/staskravets/testgit/LocationProvider.java
@@ -0,0 +1,14 @@
+package ua.staskravets.testgit;
+
+/**
+ * LocationProvider
+ *
+ * Created by kravets on 20.04.15.
+ *
+ */
+public class LocationProvider {
+
+    public LocationProvider(){
+
+    }
+}
